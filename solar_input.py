dc1deea2ae35f29d37fb9591508303fdc2bf15c3
KozlovNA <kozlov_na@phystech.edu>
2021-11-14 00:08:13 +0300

    parse fixed

--- solar_input.py
+++ solar_input.py
@@ -47,6 +47,13 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
+    star.R = line[1]
+    star.color = line[2]
+    star.m = line[3]
+    star.x = line[4]
+    star.y = line[5]
+    star.Vx = line[6]
+    star.Vy = line[7]
 
     pass  # FIXME: not done yet
 
@@ -65,6 +72,13 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
+    planet.R = line[1]
+    planet.color = line[2]
+    planet.m = line[3]
+    planet.x = line[4]
+    planet.y = line[5]
+    planet.Vx = line[6]
+    planet.Vy = line[7]
     pass  # FIXME: not done yet...
 
 
